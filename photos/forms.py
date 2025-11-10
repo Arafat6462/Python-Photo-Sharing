3766e210c92c90f791686248bdb2c09aca9d1478
Arafat Hossain <arafat6462@gmail.com>
2025-11-10 10:59:02 +0600

    enhance user interface with custom CSS

--- photos/forms.py
+++ photos/forms.py
@@ -9,6 +9,12 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = UserCreationForm.Meta.fields + ('is_creator',)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = None
+        self.fields['password2'].help_text = None
+        # self.is_creator.help_text = None
+
 class PhotoUploadForm(forms.ModelForm):
     class Meta:
         model = Photo
