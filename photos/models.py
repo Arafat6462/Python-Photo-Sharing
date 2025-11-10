799f161a5d8f6a0a6b55e97720c6bb16ab9758ec
Arafat Hossain <arafat6462@gmail.com>
2025-11-10 08:47:26 +0600

    add User and Photo models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    is_creator = models.BooleanField(default=False)
+
+class Photo(models.Model):
+    title = models.CharField(max_length=255)
+    caption = models.TextField(blank=True)
+    location = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='photos/')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='photos')
+
+    def __str__(self):
+        return self.title
--- photoshare/settings.py
+++ photoshare/settings.py
@@ -89,6 +89,8 @@ DATABASES = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+AUTH_USER_MODEL = 'photos.User'
+
 
 
 # Password validation
